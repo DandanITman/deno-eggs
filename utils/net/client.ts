6a7c90f2eb5d49b031c36978993a6d23ba6923f0
unknown <swiftsword94@gmail.com>
2020-11-11 16:53:49 -0500

    deno import system not infering type from sendrequest

--- utils/net/client.ts
+++ utils/net/client.ts
@@ -18,7 +18,7 @@ export abstract class Client{
   }
 
   
-  protected sendRequest<T>(url: string, options?: Options) {
+  protected sendRequest<T>(url: string, options?: Options): T {
     return ky.post(url, options ?? this.options).json<T>();
   }
 
